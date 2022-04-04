f0996b29da204514005d95f836428ddce941a33e
drew199321 <43482659+drew199321@users.noreply.github.com>
2022-04-03 22:49:36 -0700

    print ID to top of Login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useRef } from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
 
-export default function Login() {
+export default function Login({ onIdSubmit }) {
+    const idRef = useRef()
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        onIdSubmit(idRef.current.value)
+    }
+
   return (
-    <Container>
-        Hello Login
+    <Container className = "align-items-center d-flex" style = {{ height: '100vh' }}>
+        <Form onSubmit = {handleSubmit} className = "w-100">
+            <Form.Group>
+                <Form.Label>Enter ID</Form.Label>
+                <Form.Control type="text" ref={idRef} required></Form.Control>
+            </Form.Group>
+            <Button type="submit" style = {{marginTop: '10px', marginRight: '10px'}}>Login</Button>
+            <Button variant="secondary" style = {{marginTop: '10px', marginRight: '10px'}}>Generate New ID</Button>
+        </Form>
     </Container>
   )
 }
