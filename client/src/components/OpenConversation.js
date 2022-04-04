5286fb2c1f6b819f13764da61d584c458a0cad6c
drew199321 <43482659+drew199321@users.noreply.github.com>
2022-04-04 00:43:52 -0700

    Open Conversation begun

---
+++ client/src/components/OpenConversation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OpenConversation() {
+  return (
+    <div className="d-flex flex-column flex-grow-1">
+        Open Conversation
+        </div>
+  )
+}
