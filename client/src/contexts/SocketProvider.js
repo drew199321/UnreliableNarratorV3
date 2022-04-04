f7c94cad52b165b374a8a16844b9358fa2f30a92
drew199321 <43482659+drew199321@users.noreply.github.com>
2022-04-04 09:21:33 -0700

    base of socket implementation

--- client/src/contexts/SocketProvider.js
+++ client/src/contexts/SocketProvider.js
@@ -1,19 +1,25 @@
 import React, { useContext, useEffect, useState} from 'react'
+import io from 'socket.io-client'
 
 const SocketContext = React.createContext()
 
 export function useSocket() {
-    return useContext(SocketContent)
+    return useContext(SocketContext)
 }
 
-export function SocketProvider( id, children ) {
+export function SocketProvider({ id, children }) {
+  const [socket, setSocket] = useState()
 
     useEffect(() => {
-        const newSocket = io('http://localhost:5000', { query: {id}}, [id])
-        }
-    )
+        const newSocket = io('http://localhost:5000', 
+          { query: {id} }, [id]
+        )
+        setSocket(newSocket)
+
+        return() => newSocket.close()
+        }, [id])
   return (
-    <SocketContext.Provider> value = {socket}
+    <SocketContext.Provider value={socket}>
         {children}
     </SocketContext.Provider>
   )
