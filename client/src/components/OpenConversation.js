dbc4f28528ac50f98ec8de2c212f559714b31aa8
drew199321 <43482659+drew199321@users.noreply.github.com>
2022-04-04 01:15:55 -0700

    conversation provider work

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react'
 import { Form, InputGroup, Button } from 'react-bootstrap'
+import { useConversations } from '../contexts/ConversationsProvider'
 
 export default function OpenConversation() {
     const [text, setText] = useState('')
+    const { sendMessage, selectedConversation } = useConversations()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        sendMessage(
+            selectedConversation.recipients.map(r => r.id),
+            text
+        )
+        setText('')
+    }
 
   return (
     <div className="d-flex flex-column flex-grow-1">
         <div className = "flex-grow-1 overflow-auto">
 
         </div>
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className = "m-2">
                 <InputGroup>
                 <Form.Control 
