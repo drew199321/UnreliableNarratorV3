6f6148db3cd0b780a5e15dc5e0a149d957dfe506
drew199321 <43482659+drew199321@users.noreply.github.com>
2022-04-04 00:48:40 -0700

    Textbox Added

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, InputGroup } from 'react-bootstrap'
 
 export default function OpenConversation() {
+    const [text, setText] = useState()
+
   return (
     <div className="d-flex flex-column flex-grow-1">
-        Open Conversation
+        <div className = "flex-grow-1 overflow-auto">
+
         </div>
+        <Form>
+            <Form.Group>
+                <InputGroup>
+                <Form.Control 
+                    as="textarea" 
+                    required 
+                    value={text} 
+                    onChange={e => setText(e.target.value)}
+                    style = {{ height: '75px', resize: 'none' }}
+                />
+                </InputGroup>
+            </Form.Group>
+            
+        </Form>
+    </div>
   )
 }
